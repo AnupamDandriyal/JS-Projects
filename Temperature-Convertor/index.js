7a0f34d208b8a2849e9f17f2be2f46cd6eceb88d
Anupam Dandriyal <anupamdandriyal25@gmail.com>
2024-08-12 10:01:28 +0530

    added the functionality to the temperature calculator

--- Temperature-Convertor/index.js
+++ Temperature-Convertor/index.js
@@ -1,28 +1,46 @@
+const farenheit = document.querySelector('.farenheit');
+const celsius = document.querySelector('.celsius');
+const kelvin = document.querySelector('.kelvin')
+
+
 const temp = document.querySelectorAll('.temp');
-temp.forEach((input) => {
-  input.addEventListener('click', () => {
-    if (input.classList.contains('celsius')) {
-      inputCelsius();
+temp.forEach((inputtemp) => {
+  inputtemp.addEventListener('input', () => {
+    let val = inputtemp.value;
+    if (inputtemp.classList.contains('celsius')) {
+      inputCelsius(val);
     }
 
-    else if (input.classList.contains('farenheit')) {
-      inputFarenheit();
+    else if (inputtemp.classList.contains('farenheit')) {
+      inputFarenheit(val);
     }
     else {
-      inputKelvin();
+      inputKelvin(val);
     }
   })
 })
 
 
-function inputFarenheit() {
-  console.log('Farenheit is clicked')
+function inputFarenheit(value) {
+  let val = parseFloat(value);
+  let c = ((5 / 9) * (val - 32));
+  let k = ((5 / 9) * (val - 32) + 273.15);
+  celsius.value = c.toFixed(2);
+  kelvin.value = k.toFixed(2);
 }
 
-function inputCelsius() {
-  console.log('Celsius is clicked')
+function inputCelsius(value) {
+  let val = parseFloat(value);
+  let f = ((9 / 5) * val) + 32;
+  let k= (val + 273.15);
+  farenheit.value = f.toFixed(2);
+  kelvin.value = k.toFixed(2);
 }
 
-function inputKelvin(){
-  console.log('Kelvin is clicked')
+function inputKelvin(value){
+  let val = parseFloat(value);
+  let c = (val - 273.15);
+  let f = ((9 / 5) * (val - 273.15)) + 32;
+  celsius.value = c.toFixed(2);
+  farenheit.value =f.toFixed(2)
 }
