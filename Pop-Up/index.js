9f1e1f6c04cb95cfb6d0b7671d3f27c2cd3d11e6
Anupam Dandriyal <anupamdandriyal25@gmail.com>
2024-08-16 00:27:11 +0530

    added the functionality in pop up project and completed it

--- Pop-Up/index.js
+++ Pop-Up/index.js
@@ -2,12 +2,16 @@ const btnEl = document.querySelector('.ok');
 const submitEl = document.querySelector('.submit');
 const popupEl = document.querySelector('.popup');
 btnEl.addEventListener('click', () => {
-  popupEl.style.display = 'none';
+  popupEl.style.top = `0`;
+  popupEl.style.transform = 'translate(-50%,-50%) scale(0.1)'
+  popupEl.style.visibility = 'hidden';
   submitEl.style.display = 'block';
 });
 
 
 submitEl.addEventListener('click', () => {
-  popupEl.style.display = 'block';
+  popupEl.style.visibility = 'visible';
+  popupEl.style.top = `50%`;
+  popupEl.style.transform = 'translate(-50%,-50%) scale(1)'
   submitEl.style.display = 'none';
 });
