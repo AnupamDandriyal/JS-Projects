d52e62086683dd5a475dfe017d133657f23220b6
Anupam Dandriyal <anupamdandriyal25@gmail.com>
2024-08-16 20:03:02 +0530

    completed the password strength

--- password-strength/index.js
+++ password-strength/index.js
@@ -1,27 +1,38 @@
 const input = document.querySelector('input');
 const strength = document.querySelector('.strength');
-const containerEl = document.querySelector('.container');
+const inputEl = document.querySelector('.input');
+const check = document.querySelector('.check');
+check.addEventListener('click', () => {
+  if (input.type === 'password') {
+    input.type = 'text';
+  }
+  else {
+    input.type = 'password'
+  }
+})
 input.addEventListener('keyup', () => {
   const length = input.value.length;
-  if (length > 0 && length <= 5) {
+  if ((length > 0 && length <= 5) && (/[A-Za-z]/.test(input)) ){
     strength.innerHTML = 'weak';
     strength.style.color = 'red';
-    containerEl.style.border = '1px solid red';
+    inputEl.style.border = '2px solid red';
   }
 
-  else if (length > 5 && length <= 10) {
+  else if ((length > 5 && length <= 8) && (/[A-Za-z0-9]/.test(input))) {
     strength.innerHTML = 'moderate';
     strength.style.color = 'sandybrown';
-    containerEl.style.border = '1px solid sandybrown';
+    inputEl.style.border = '2px solid sandybrown';
   }
 
-  else if(length>10) {
+  else if((length>8) && (/[A-Za-z0-9~!@#$%^&*()-+]/.test(input))) {
     strength.innerHTML = 'strong';
     strength.style.color = 'green';
-    containerEl.style.border = '1px solid green';
+    inputEl.style.border = '2px solid green';
   }
   else {
     strength.innerHTML = '';
-    containerEl.style.border = '1px solid grey';
+    inputEl.style.border = '2px solid grey';
   }
 })
+
+
