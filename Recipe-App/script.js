baba78ac760c04e5ac7764e20fa2b0e8f6686144
Anupam Dandriyal <anupamdandriyal25@gmail.com>
2024-08-21 01:45:45 +0530

    done with the recipee app

--- Recipe-App/script.js
+++ Recipe-App/script.js
@@ -4,26 +4,31 @@ const mainHeading = document.querySelector('.main h4');
 const recipes = document.querySelector('.recipes');
 const main = document.querySelector('.main');
 
+inputEl.addEventListener('keyup', () => {
+  let instructions = document.querySelector('.instructions')
+  instructions.style.display = 'none';
+})
 
 searchBtn.addEventListener('click', () => {
   const food = (inputEl.value);
   inputEl.value = '';
-  mainHeading.innerHTML = 'Great recipes are on their way......';
-  setTimeout(()=>getRecipes(food), 2000);
-  
+  mainHeading.innerHTML = 'Have Some Patience, Great recipes are on their way......';
+  setTimeout(() => getRecipes(food), 2000);
 })
 
 
 async function getRecipes(food) {
+ 
+  mainHeading.innerHTML = '';
+  recipes.innerHTML = '';
+  recipes.style.display = 'grid';
   try {
     const response = await fetch(`https://themealdb.com/api/json/v1/1/search.php?s=${food}`);
     const data = await response.json();
     const dishes = data.meals;
-    mainHeading.innerHTML = '';
-    recipes.innerHTML = '';
     console.log(dishes);
-    for (dish of dishes) {
-      console.log(dish.strMeal);
+    for (let dish of dishes) {
+      /*    console.log(dish.strMeal); */
       let item = document.createElement('div');
       item.className = 'item';
       let img = document.createElement('img');
@@ -32,13 +37,16 @@ async function getRecipes(food) {
       title.innerHTML = dish.strMeal;
       let type = document.createElement('p');
       type.className = 'type'
-      type.innerHTML = dish.strArea;
+      type.innerHTML = `${dish.strArea} dish`;
       let category = document.createElement('p');
-      category.innerHTML = dish.strCategory;
+      category.innerHTML = `Category: ${dish.strCategory}`;
       category.className = 'category';
       let btn = document.createElement('button');
       btn.className = 'view';
       btn.innerHTML = 'Get Recipe';
+      btn.addEventListener('click', () => {
+        recipePopUp(dish);
+      })
       item.appendChild(img);
       item.appendChild(title);
       item.appendChild(type);
@@ -54,38 +62,43 @@ async function getRecipes(food) {
   
 }
 
-
-
-/*  btn.addEventListener('click', () => {
-        recipes.style.filter = 'opacity(10%)';
-        let instructions = document.createElement('div');
-        instructions.className = 'instructions';
-        let indegredients = document.createElement('h3');
-        indegredients.innerHTML = dish.strMeal
-        let steps = document.createElement('h3');
-        steps.innerHTML = 'Steps';
-        let close = document.createElement('button');
-        close.className = 'close';
-        close.addEventListener('click', () => {
-          recipes.style.filter = 'opacity(100%)';
-          instructions.style.display = 'none';
-        })
-        instructions.appendChild(indegredients);
-        instructions.appendChild(steps);
-        instructions.appendChild(close);
-      }) */
-
-/* const instructions = document.querySelector('.instructions');
-
-const view = document.querySelector('.view');
-const close = document.querySelector('.close');
-view.addEventListener('click',()=> {
-  recipes.style.filter = 'opacity(10%)';
+function recipePopUp(dish) {
+  const instructions = document.querySelector('.instructions');
+  recipes.style.display = 'none';
   instructions.style.display = 'block';
-})
+  instructions.innerHTML = `
+                  <button class="close">&times;</button>
+                  <h2>${dish.strMeal}</h2>
+                  <p class="ingredients">
+                    <h3>Ingredients</h3>
+                    <ul> ${fetchIngredients(dish)}</ul>
+                  </p>
+                  <p class="procedure">
+                    <h3>Instructions</h3>
+                    ${dish.strInstructions}
+                  </p>                      
+  `
+
+  let close = document.querySelector('.close');
+  close.addEventListener('click', () => {
+    recipes.style.display = 'grid';
+    instructions.style.display = 'none';
+  })
+}
 
 
-close.addEventListener('click',()=> {
-  recipes.style.filter = 'opacity(100%)';
-  instructions.style.display = 'none';
-}) */
+function fetchIngredients(dish) {
+  let ingredientsList = '';
+  for (let i = 1; i <= 20; i++){
+    const ingredient = dish[`strIngredient${i}`];
+    if (ingredient) {
+      const measure = dish[`strMeasure${i}`];
+      ingredientsList +=`<li>${measure} - ${ingredient}</li>`
+    }
+    else {
+      break;
+    }
+  }
+  return ingredientsList;
+}
+
